66963da06d731afc885cb1e187f9c23d59a37f2a
gionata <gionata@pop-os.localdomain>
2025-08-10 23:45:26 +0200

    Fixing issues for Melpa

--- eplotly-autoloads.el
+++
@@ -1,22 +0,0 @@
-;;; eplotly-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-(add-to-list 'load-path (directory-file-name
-                         (or (file-name-directory #$) (car load-path))))
-
-
-;;;### (autoloads nil "eplotly" "eplotly.el" (0 0 0 0))
-;;; Generated autoloads from eplotly.el
-
-(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "eplotly" '("eplotly-")))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; eplotly-autoloads.el ends here
